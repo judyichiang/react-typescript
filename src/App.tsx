54156b27b09aad34a635fd3fa863972520ed5878
Judy Chiang <jiyc1119@gmail.com>
2020-05-18 22:37:51 -0700

    update to tsx

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    let firstValue: string = 'Manny';
+
+
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            The value {firstValue} is of {typeof firstValue} type!
+        </p>
+
+        </header>
+      </div>
+    );
+  }
+
+}
+
+export default App;
