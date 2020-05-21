40d46b64b784e84dc397aa7a3ad8f3759c2a3d46
Judy Chiang <jiyc1119@gmail.com>
2020-05-21 13:38:17 -0700

    material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Checkout from './Components/Checkout'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Checkout />
   </React.StrictMode>,
   document.getElementById('root')
 );
