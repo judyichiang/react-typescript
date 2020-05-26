be5dace2861e59853b87ba2b2366f8414a5c64e6
Judy Chiang <jiyc1119@gmail.com>
2020-05-25 20:28:16 -0700

    interface and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Message from './message'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Message from "./message";
 
-class App extends Component <any> {
-
-
-  //b/c using class Component , it comes with life cycles
-  //will run as your component will mount
-  componentWillMount(){
-    console.log('Almost there ..')
-  }
-
-  //func runs after mounted
-  componentDidMount(){
-    console.log('Finally...hello')
-  }
-
-
+class App extends Component<any> {
   render() {
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Message message = 'This is a simple message'/>
-
-
+          <Message name="User" message="This is a simple message" />
         </header>
       </div>
     );
   }
-
 }
 
 export default App;
